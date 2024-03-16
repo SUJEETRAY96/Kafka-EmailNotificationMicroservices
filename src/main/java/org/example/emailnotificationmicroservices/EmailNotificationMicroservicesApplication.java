37e808d5da07002d934d196f6ac90fa853d18737
SUJEET KUMAR RAY <sujeetray96@gmail.com>
2024-03-16 21:01:41 +0530

    Consumer

---
+++ src/main/java/org/example/emailnotificationmicroservices/EmailNotificationMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailnotificationmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailNotificationMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailnotificationmicroservices/EmailNotificationMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailnotificationmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
